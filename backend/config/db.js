cfeeab443196890911d4055007c402204b7395db
Rishab <rishabdakhale17@gmail.com>
2026-05-09 13:26:20 +0530

    Import Live DB

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,9 @@
 import mysql from "mysql2";
 
 export const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "blog_website",
-  password: "root",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: 3306,
 });
