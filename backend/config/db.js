8ed9100fced85606bb55613c0d1b15ca6c748fa3
Rishab <rishabdakhale17@gmail.com>
2026-05-20 16:40:28 +0530

    connect a new db

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,10 +5,9 @@ dotenv.config();
 
 const { Pool } = pkg;
 
-export const connection = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: 5432,
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
